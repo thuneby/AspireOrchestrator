b9e2b6a3e76145bbdf924c6eda968e36f852fd03
John Thuneby <thuneby@yahoo.dk>
2025-08-12 21:32:19 +0200

    Added Parse.WebApi Swagger

---
+++ src/AspireOrchestrator.Parsing.WebApi/Controllers/ParseController.cs
@@ -0,0 +1,33 @@
+﻿using AspireOrchestrator.Core.OrchestratorModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspireOrchestrator.Parsing.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ParseController(ILoggerFactory loggerFactory) : ControllerBase
+    {
+        private readonly ILogger<ParseController> _logger = loggerFactory.CreateLogger<ParseController>();
+
+        [HttpGet("[action]")]
+        public ActionResult Welcome()
+        {
+            return Ok("Welcome to the parse controller");
+        }
+
+        [HttpPost("[action]")]
+        public async Task<ActionResult<EventEntity>> ParseAsync([FromBody] EventEntity eventEntity)
+        {
+            _logger.LogInformation("Received parse request: {@Request}", eventEntity);
+
+            // Simulate parsing logic
+
+            eventEntity.StartTime = DateTime.Now;
+            eventEntity.Result = "Parsing completed successfully";
+            eventEntity.EventState = EventState.Completed;
+            eventEntity.ExecutionCount++;
+            eventEntity.EndTime = DateTime.Now.AddSeconds(1); // Simulate processing time
+            return eventEntity;
+        }
+    }
+}
--- src/AspireOrchestrator.Parsing.WebApi/Program.cs
+++ src/AspireOrchestrator.Parsing.WebApi/Program.cs
@@ -8,17 +8,32 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
 var app = builder.Build();
 
 app.MapDefaultEndpoints();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline and seed database.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger(c => { c.OpenApiVersion = Microsoft.OpenApi.OpenApiSpecVersion.OpenApi2_0; });
+    app.UseSwaggerUI();
+
+    //using (var scope = app.Services.CreateScope())
+    //{
+    //    var context = scope.ServiceProvider.GetRequiredService<OrchestratorContext>();
+    //    context.Database.EnsureCreated();
+    //}
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
